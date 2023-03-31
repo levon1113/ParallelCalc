47ca314161930fa6fbda58fb9b3f0b21d3352400
Levon Fermanyan <fermanyanlevon@gmail.com>
2023-03-31 15:16:52 +0400

    Add project files.

---
+++ ParallelCalc/ParallelCalc.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThreadState = System.Threading.ThreadState;
+
+namespace ParallelCalc
+{
+    internal class ParallelCalc
+    {
+        private long Result;
+        public long Min { get; set; }
+        public long Max { get; set; }
+        public int N { get; set; }
+
+        public ParallelCalc()
+        {
+
+        }
+
+        public ParallelCalc(long min, long max, int n)
+        {
+            Min = min;
+            Max = max;
+            N = n;
+            Result = 0;
+        }
+
+        public long Run(out long elapsed)
+        {
+            
+            Thread[] threads = new Thread[N];
+            long m = Max - Min;
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            
+            for (int i = 0; i < N; i++)
+            {
+                long min=0, max=0;
+                min = Min + i * (m / N) + 1;
+
+                if (m % N != 0 && i == N - 1)
+                {
+                    max = Min + (i + 1) * (m / N) + m%N;
+                }
+                else
+                {
+                    max = Min + (i + 1) * (m / N);
+                }
+                threads[i] = new Thread(()=>Calc(min,max));
+                threads[i].Start();
+            }
+            
+            while (CheckThreads(threads)) ;
+
+            stopwatch.Stop();
+            
+            elapsed = stopwatch.ElapsedMilliseconds;
+            
+            return Result + Min;
+        }
+
+        private bool CheckThreads(Thread[] threads)
+        {
+            for (int i = 0; i < threads.Length; i++)
+            {
+                if (threads[i].ThreadState == ThreadState.Running)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public void Calc(long min, long max)
+        {
+            long s = 0;
+            for (long i = min; i <= max; i++)
+            {
+                s += i;
+            }
+            Result += s;
+        }
+
+    }
+}
---
+++ ParallelCalc/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ParallelCalc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Minimum number -> ");
+            long min = long.Parse(Console.ReadLine());
+            while (min < 1)
+            {
+                Console.Write("Must be higher than or equal to 1 -> ");
+                min = long.Parse(Console.ReadLine());
+
+            }
+            Console.Write("Maximum number -> ");
+            long max = long.Parse(Console.ReadLine());
+            while (max<min)
+            {
+                Console.Write("Must be higher than or equal to minimum number -> ");
+                max = long.Parse(Console.ReadLine());
+
+            }
+            Console.Write("Parts -> ");
+            int parts = int.Parse(Console.ReadLine());
+            while (parts <= 0)
+            {
+                Console.Write("Must be higher than 0 -> ");
+                max = long.Parse(Console.ReadLine());
+
+            }
+            ParallelCalc parallelCalc = new ParallelCalc(min,max,parts);
+            long res = parallelCalc.Run(out long elapsed);
+            Console.WriteLine($"Result - {res}, Time Elapsed - {elapsed}");
+        }
+    }
+}
